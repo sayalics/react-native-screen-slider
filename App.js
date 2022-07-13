5e60a8cad6910f6ada3be73c954d257bee0c2d65
S@y@li1431997 <sayali.sonawane@mindbowser.com>
2022-07-13 17:53:13 +0530

    Silder Code Added.

--- App.js
+++ App.js
@@ -1,112 +1,255 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
-} from 'react-native';
+  Text,
+  StatusBar,
+  Image,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+  const [isLast, setIsLast] = useState(false);
+  const [_index, _setIndex] = useState(0);
+  const slider = useRef();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (_index < 3){
+      _setIndex(_index + 1);
+      slider.current.goToSlide(_index + 1, true)
+      }
+    },5000)
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    return () => clearInterval(interval);
+  },[_index]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+
+
+  const slides = [
+    {
+      key: "s1",
+      dot: true,
+    },
+    {
+      key: "s2",
+      dot: true,
+    },
+    {
+      key: "s3",
+      dot: false,
+    },
+    {
+      key: "s4",
+      dot: false,
+    },
+  ]
+
+  const _renderIntroScreen = () => {
+    return (
+      <View style={{ flex:1, backgroundColor:"#7dd195" }}>
+          <View style={styles.headingView}>
+              <Text style={styles.title}>
+                  {"DEVELOPMENT \nACTIVITIES"}
+              </Text>
+              <Text style={styles.subTitle}>To boost your baby's growth!</Text>
+          </View>
+          <View style={{flex:0.6}}>
+              <View style={styles.mobileView}>
+              <Image
+              style={styles.gif}
+              resizeMode={"contain"}
+              source={require("./rubicks-cube-game.gif")}
+              />              
+              </View>
+          </View>
+      </View>
+    );
+  };
+
+  const _renderIntro_2Screen = () => {
+    return (
+        <View style={{ flex:1, backgroundColor:"#72b5e8" }}>
+            <View style={styles.headingView}>
+                <Text style={styles.title}>
+                    {"DEVELOPMENT \nREPORTS"}
+                </Text>
+                <Text style={styles.subTitle}>
+                    To track your baby's growth!
+                </Text>
+            </View>
+            <View style={{flex:0.6}}>
+                <View style={styles.mobileView}>
+                <Image
+                  style={styles.gif}
+                  resizeMode={"contain"}
+                  source={require("./rubicks-cube-game.gif")}
+                  />              
+                </View>
+            </View>
+        </View>
+    );
+  };
+
+  const _renderIntro_3Screen = () => {
+    return (
+        <View style={{ flex:1, backgroundColor:"#faaa61" }}>
+            <View style={styles.headingView}>
+                <Text style={styles.title}>
+                    {"RECIPES, STORIES \n& ARTICLES"}
+                </Text>
+                <Text style={styles.subTitle}>
+                    One stop for all your needs!
+                </Text>
+            </View>
+            <View style={{flex:0.6}}>
+                <View style={styles.mobileView}>
+                <Image
+                style={styles.gif}
+                resizeMode={"contain"}
+                source={require("./rubicks-cube-game.gif")}
+                />              
+                </View>
+            </View>
+        </View>
+    );
+  };
+
+  const _renderIntro_4Screen = () => {
+    return (
+        <View style={{ flex:1, backgroundColor:"#f9bbbb" }}>
+            <View style={styles.headingView}>
+                <Text style={[styles.title,{ paddingHorizontal:30}]}>
+                    {"PARENTING \nCLUB"}
+                </Text>
+                <Text style={styles.subTitle}>
+                    {"Learn & share experience, anonymously"}
+                </Text>
+            </View>
+            <View style={{flex:0.6}}>
+                <View style={styles.mobileView}>
+                <Image
+                style={styles.gif}
+                resizeMode={"contain"}
+                source={require("./rubicks-cube-game.gif")}
+                />              
+                </View>
+            </View>
+        </View>
+    );
+  };
+
+  const _renderItem = ({ item, index }) => {
+    return (
+      <View style={styles.container}>
+        {_index == 0 ? _renderIntroScreen() : null}
+        {_index == 1 ? _renderIntro_2Screen() : null}
+        {_index == 2 ? _renderIntro_3Screen() : null}
+        {_index == 3 ? _renderIntro_4Screen() : null}
+      </View>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{flex:1, height: "100%", width: "100%" }}>
+      <StatusBar hidden={true} />
+      <AppIntroSlider
+        ref={(ref) => (slider.current = ref)}
+        data={slides}
+        renderItem={_renderItem}
+        onSlideChange={(e) => e === 3 && setIsLast(true)}
+        dotStyle={[
+          styles.dotStyle,
+        ]}
+        activeDotStyle={{ backgroundColor: "#6185c6", outerWidth:300, height:6, width:54 }}
+        showSkipButton={false}
+        showNextButton={false}
+        showDoneButton={false}
+      />
+      {/* )} */}
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    container: {
+        flex: 1,
+        height: "100%",
+        width: "100%",
+        backgroundColor: "#FFFFFF",
+    },
+    ImgStyle: {
+        width: "100%",
+        height: "99%",
+        resizeMode: "contain",
+    },
+    dotStyle: {
+        height: 6,
+        width: 6,
+        backgroundColor: "#6185c6",
+    },
+    closeImageStyle: {
+        position: "absolute",
+        top: 50,
+        left: 20,
+        padding: 6,
+        width: "10%",
+    },
+    gif: {        
+      flex:1, 
+      width:Dimensions.get("screen").width - 100, 
+      alignSelf:'center', 
+    },
+    closeImage: {
+        width: 25,
+        height: 25,
+    },
+    textStyle: {
+        borderRadius: 50,
+        backgroundColor: "#DCDCDC",
+        alignSelf: "center",
+        position: "absolute",
+        bottom: 50,
+        alignSelf: "center",
+        width: "70%",
+        paddingHorizontal: 10,
+        paddingVertical:10,
+        elevation:5
+    },
+    text: {
+        alignSelf: "center",
+        color: "#57b5bc",
+        fontSize: 15,
+    },
+    headingView:{
+        flex:0.4, 
+        alignItems:"center", 
+        justifyContent:'flex-end', 
+        paddingBottom:30,
+    },
+    title:{
+        fontSize:35, 
+        textAlign:'center', 
+        color:"#4D4D4F",
+        paddingHorizontal:20
+    },
+    subTitle:{
+        fontSize:16, 
+        marginTop:5, 
+        textAlign:'center', 
+        color:"#4D4D4F",
+        paddingHorizontal:20
+    },
+    mobileView:{
+        backgroundColor:"#FFFFFF", 
+        overflow:'hidden', 
+        flex:1, 
+        width:Dimensions.get("screen").width - 80, 
+        alignSelf:'center', 
+        borderTopLeftRadius:20, 
+        borderTopRightRadius:20,
+    },
 });
 
 export default App;
